fb2a1d31f35e067f748b62056309b8010441f307
John <jfranti@gmail.com>
2016-10-16 18:35:50 -0700

    make it work

--- app/app.php
+++ app/app.php
@@ -1,7 +1,11 @@
-<?
+<?php
     require_once __DIR__."/../vendor/autoload.php";
 
     $app = new Silex\Application();
+
+    $app->get("/", function() {
+        return "This is the index page!";
+    });
 
     $app->get("/hello", function() {
         return "Hello friend!";
